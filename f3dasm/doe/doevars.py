5788b95d530f2711897cb4886810976890a44cb6
Manuel G. Garcia <gilbert.mg22@gmail.com>
2021-12-08 17:23:32 +0100

    add grup id to doe vars defined as a tupple

--- f3dasm/doe/doevars.py
+++ f3dasm/doe/doevars.py
@@ -25,7 +25,7 @@ def print_variables(dictionary:dict):
 def find_sampling_vars(doe_vars: dict):
     """
     Find names of DoeVars that contain a definiton of a sampling method.
-    WARNING: only 3 levels of nesting in the inputs are checked
+    WARNING: only 3 levels of nesting are checked in the input.
     Args:
         doe_vars (dict): variables defining a design of experiment
     Returns:
@@ -38,8 +38,12 @@ def find_sampling_vars(doe_vars: dict):
     elements_with_functions = [] # list of names
     # loop over variables at the top level of serialization
     for key, value in vars_level1.items():
+        # id to keep track of which variable belog to the same group
+        # group ids will will start with f3dams# followed by the value group_id
+        group_id = 1
         if isinstance(value, SamplingMethod):
             elements_with_functions.append(key)
+
         # case for OR operator, 
         # variable grouped as a tupple are combined differently
         # then ungrouped variables, see create_combinations()
@@ -49,10 +53,10 @@ def find_sampling_vars(doe_vars: dict):
                 g_vars = g_df.to_dict(orient='records')[0]
                 for g_key, g_value in g_vars.items():
                     if isinstance(g_value, SamplingMethod):
-                        elements_with_functions.append(key+'.'+g_key)
-                # elements_with_functions.append('true')
+                        elements_with_functions.append('f3dasm#'+str(group_id)+'.'+key+'.'+g_key)
         else:
             continue
+        group_id+=1
     # TODO: find a generic solution this function. So that it works for 
     # any level of nesting.
     return elements_with_functions
@@ -102,13 +106,7 @@ class DoeVars:
         """Apply sampling method to sampling variables, combines sampled value and fixed-values,
         and produces a pandas data frame with all combinations.
         """
-
-        # TODO: The case of Fs needs its own way to construct the array to fetch the names of the data columns.
-        # try to implement a different case in the sampling method based on the number of dimmension, if more than one
-        # then inner keys shoul become data columns
-
-        # convert sampling results back to dict, before doing combination, and spliting the data columns
-
+        # make copy of doe variables
         doe_vars = copy.deepcopy(self.variables)
         
         print('sampling var:', self.sampling_vars)
@@ -116,16 +114,16 @@ class DoeVars:
         for var in self.sampling_vars:
             inner_vars = var.split('.') 
             print('inner vars:', inner_vars)
-            if len(inner_vars) == 1:
-                print("var-ranges:", doe_vars[var].sampling_ranges.keys())
-                doe_vars[var] = 1# samples_to_dict( doe_vars[var].compute_sampling(), doe_vars[var].sampling_ranges.keys())
-                print('as dict', doe_vars[var])
-            elif len(inner_vars) == 2:
-                doe_vars[inner_vars[0]][inner_vars[1]] = doe_vars[inner_vars[0]][inner_vars[1]].compute_sampling()
-            elif len(inner_vars) == 3:
-                doe_vars[inner_vars[0]][inner_vars[1]][inner_vars[2]] = doe_vars[inner_vars[0]][inner_vars[1]][inner_vars[2]].compute_sampling()
-            else:
-                raise SyntaxError("DoeVars definition contains too many nested elements. A max of 3 is allowed")
+            # if len(inner_vars) == 1:
+            #     print("var-ranges:", doe_vars[var].sampling_ranges.keys())
+            #     doe_vars[var] = 1# samples_to_dict( doe_vars[var].compute_sampling(), doe_vars[var].sampling_ranges.keys())
+            #     print('as dict', doe_vars[var])
+            # elif len(inner_vars) == 2:
+            #     doe_vars[inner_vars[0]][inner_vars[1]] = doe_vars[inner_vars[0]][inner_vars[1]].compute_sampling()
+            # elif len(inner_vars) == 3:
+            #     doe_vars[inner_vars[0]][inner_vars[1]][inner_vars[2]] = doe_vars[inner_vars[0]][inner_vars[1]][inner_vars[2]].compute_sampling()
+            # else:
+            #     raise SyntaxError("DoeVars definition contains too many nested elements. A max of 3 is allowed")
     
         # print('sampling doe', doe_vars)
         # combinations
@@ -159,7 +157,7 @@ if __name__ == '__main__':
 
 
 
-     ### COMBINATIONS
+     ### COMBINATIONS and sampling
     vars = {'Fs': SalibSobol(4, {'F11':[10, 20], 'F12':[-0.1,0.15], 'F22':[-0.15, 1]}), 
             'R': [0.3, 0.5], 
             'particle': ({'P': [100, 200]}, {'Q': [15, 25]} ) 
